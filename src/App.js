5ed89d2d485f86049e840359d501db618d1c6453
Anurag <danurag1906@gmail.com>
2022-08-30 12:29:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let list1 = [
+    "milk",
+    "coffee",
+    "orange",
+    "breads",
+    "banana",
+    "mango",
+    "butter",
+  ];
+
+  var [myarray, setmyarray] = new useState([]);
+  var [myarray1, setmyarray1] = new useState([]);
+
+  const onClick = () => {
+    var item = list1[Math.floor(Math.random() * list1.length)];
+    myarray.push(item);
+    setmyarray((myarray) => [...myarray]);
+    setmyarray1(myarray);
+    // console.log(myarray);
+  };
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+
+    myarray = myarray1.filter((item) => {
+      return item.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+    });
+    setmyarray(myarray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <form className="d-flex col-md-5 mx-5 my-3" role="search">
+          <input
+            className="form-control me-2"
+            onChange={filter}
+            type="search"
+            placeholder="Search"
+            aria-label="Filter"
+          />
+
+          <div className="container">
+            <button type="button" onClick={onClick} className="btn btn-primary">
+              +
+            </button>
+          </div>
+        </form>
+
+        <div className="a mx-5">
+          {myarray.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
